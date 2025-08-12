f8b519b3ea30f22313b705c6f20e59af30b48519
Silva72kk <silvafelixbarbosa614@gmail.com>
2025-08-12 12:15:36 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,73 @@
+criaCartao(
+    'Programação',
+    'Qual a função do motor de um carro?'
+    'O motor é o responsável por transformar o combustível em energia mecânica, fazendo o carro se mover.
+'
+)
+
+criaCartao(
+    'Geografia',
+    'O que é o câmbio de um carro?',
+    'É o sistema que regula a relação entre a rotação do motor e a velocidade das rodas, permitindo mudanças de marcha.
+
+)
+
+criaCartao(
+    'Programação',
+    'Para que serve o óleo do motor?',
+    'Serve para lubrificar as peças móveis do motor, reduzir o atrito e evitar o superaquecimento.
+'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    ' O que acontece se o carro andar sem água no radiador?'
+    'O motor pode superaquecer, causando danos graves e até a queima do motor.
+'
+)
+criaCartao(
+    'Geografia',
+    'Qual a diferença entre carro manual e automático?',
+    'O carro manual exige que o motorista troque as marchas, enquanto o automático faz isso sozinho.
+.
+
+)
+
+criaCartao(
+    'Programação',
+    '. O que é a suspensão do carro?',
+    'É o conjunto de peças que absorve os impactos do solo e garante conforto e estabilidade.
+.
+'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'O que significa ABS nos freios?'
+    'É um sistema que evita o travamento das rodas durante a frenagem, aumentando a segurança.
+
+
+
+criaCartao(
+    'Geografia',
+    ' Por que é importante calibrar os pneus regularmente?',
+    'Pneus calibrados melhoram a estabilidade, economia de combustível e a durabilidade dos pneus.
+
+.
+
+)
+
+criaCartao(
+    'Programação',
+    ' O que é o consumo médio de um carro?',
+    'É a quantidade de combustível que o carro consome por quilômetro rodado, normalmente medida em km/l.
+.
+.
+'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    '. O que faz o catalisador do escapamento?'
+    'Reduz a emissão de gases poluentes liberados pelo motor na atmosfera.
+'
